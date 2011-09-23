e152d49e042719168c0bf02e0face960013ccae9
nils <nils@cpassman.org>
2011-09-23 09:48:47 +0000

    RC1 branche

---
+++ sources/downloadFile.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file 		downloadFile.php
+ * @author		Nils Laumaillé
+ * @version 	2.0
+ * @copyright 	(c) 2009-2011 Nils Laumaillé
+ * @licensing 	CC BY-ND (http://creativecommons.org/licenses/by-nd/3.0/legalcode)
+ * @link		http://cpassman.org
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+session_start();
+if (!isset($_SESSION['CPM'] ) || $_SESSION['CPM'] != 1)
+	die('Hacking attempt...');
+
+header("Content-disposition: attachment; filename=".rawurldecode($_GET['name']));
+header("Content-Type: application/force-download");
+header("Content-Transfer-Encoding: ".$_GET['type']."\n");
+header("Pragma: no-cache");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0, public");
+header("Expires: 0");
+readfile($_GET['path']);
+?>
